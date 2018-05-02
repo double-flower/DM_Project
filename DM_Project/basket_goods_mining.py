ff66c8bf5df4948429cff0e97262707b5d52cc9e
double-flower <jianhua-gao@foxmail.com>
2018-05-02 20:44:30 +0800

    add the visualized section

--- DM_Project/basket_goods_mining.py
+++ DM_Project/basket_goods_mining.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 import os
+import numpy as np
 from collections import defaultdict, Iterable
 from Apriori import Apriori
 from pandas import  DataFrame
@@ -16,32 +17,47 @@ csv_path = 'goods.csv'
 # data_merged.name = data_merged.name.str.replace("\"","")
 # data_merged['name'].to_csv(csv_path)
 # dataset = csv.reader(open(csv_path, "r"))
-# minSup = 0.03
+# minSup = 0.04
 # minConf = 0.88
+# fig_freq_name = "Figures/department_freq_bar.png"
+# fig_rule_name = "Figures/department_rule_bar.png"
+# fig_freq_title = "Frequent Sets of Department"
+# fig_rule_title = "Association Rules of Department"
 
 # handle aisle.csv
-# result_path = 'aisle_result.txt'
-# data = pd.read_csv('aisle.csv')
-# data_merged = data.groupby('order_id').agg({'name':lambda x: ','.join(x)})
-# data_merged.name = data_merged.name.str.replace("\"","")
-# data_merged['name'].to_csv(csv_path)
-# dataset = csv.reader(open(csv_path, "r"))
-# minSup = 0.02
-# minConf = 0.7
-
-# handle product.csv
-result_path = 'product_result.txt'
-data = pd.read_csv('product.csv')
+result_path = 'aisle_result.txt'
+data = pd.read_csv('aisle.csv')
 data_merged = data.groupby('order_id').agg({'name':lambda x: ','.join(x)})
 data_merged.name = data_merged.name.str.replace("\"","")
 data_merged['name'].to_csv(csv_path)
 dataset = csv.reader(open(csv_path, "r"))
-minSup = 0.01
-minConf = 0.1
+minSup = 0.045
+minConf = 0.7
+fig_freq_name = "Figures/aisle_freq_bar.png"
+fig_rule_name = "Figures/aisle_rule_bar.png"
+fig_freq_title = "Frequent Sets of isle"
+fig_rule_title = "Association Rules of Aisle"
+
+
+# handle product.csv
+# result_path = 'product_result.txt'
+# data = pd.read_csv('product.csv')
+# data_merged = data.groupby('order_id').agg({'name':lambda x: ','.join(x)})
+# data_merged.name = data_merged.name.str.replace("\"","")
+# data_merged['name'].to_csv(csv_path)
+# dataset = csv.reader(open(csv_path, "r", encoding="UTF-8"))
+# minSup = 0.01
+# minConf = 0.1
+# fig_freq_name = "Figures/product_freq_bar.png"
+# fig_rule_name = "Figures/product_rule_bar.png"
+# fig_freq_title = "Frequent Sets of Product"
+# fig_rule_title = "Association Rules of Product"
 
 # handle order_products__train.csv
 # result_path = 'product_result.txt'
 # data = pd.read_csv('order_products__train.csv')
+
+
 # data_merged = data.groupby('order_id')['product_id'].apply(list).to_frame()
 # data_merged['product_id'].to_csv(csv_path)
 # dataset = csv.reader(open(csv_path, "r"))
@@ -75,6 +91,7 @@ fp = open(result_path,'a')
 fp.writelines("Number of transactions:"+str(len(a.transList))+"\n")
 fp.writelines("Min Support:"+str(minSup)+" Min Confidence:"+str(minConf)+"\n")
 fp.writelines("Frequent Itemsets:\n")
+
 for k, item in frequentItemsets.items():
     for i in item:
         # if k >= 2:
@@ -84,8 +101,13 @@ for k, item in frequentItemsets.items():
         df_fis = df_fis.append({"rules":i,"support":round(a.support(a.freqList[i]),4)}, ignore_index=True)
 
 fp.writelines("There are "+str(count_i)+" frequent itemsets\n")
-df_fis.plot(kind = 'bar')
-plt.savefig("fis.png")
+
+df_sort = df_fis.sort_values(by='support', ascending=False)
+df_sort.head(15).plot(kind="bar")
+plt.xlabel("frequentset")
+plt.ylabel("support")
+plt.title(fig_freq_title)
+plt.savefig(fig_freq_name)
 plt.clf()
 
 # Mining the confident association rules
@@ -102,6 +124,10 @@ for i, rule in enumerate(rules):
 fp.writelines("There are "+str(count_r)+" confident rules\n")
 fp.close()
 print("There are", count_r, "confident rules\n")
-df_car["confidence"].plot(kind = 'bar')
-plt.savefig("car.png")
+
+df_car["confidence"].dropna().plot(kind = 'bar')
+plt.xlabel("rules")
+plt.ylabel("confidence")
+plt.title(fig_rule_title)
+plt.savefig(fig_rule_name)
 plt.clf()
